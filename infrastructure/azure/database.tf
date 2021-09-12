5dede20edb521837090846c832a47f29ae877b62
Jim Counts <jamesrcounts@outlook.com>
2021-09-12 00:33:15 +0000

    Add connection strings to configuration

--- infrastructure/azure/database.tf
+++ infrastructure/azure/database.tf
@@ -1,12 +1,16 @@
-resource "azurerm_mssql_database" "db" {
-  for_each = {
-    "std"     = module.sql_standard.server_id
-    "proxy"   = module.sql_proxy.server_id
-    "private" = module.sql_private.server_id
+locals {
+  server = {
+    "std"     = module.sql_standard
+    "proxy"   = module.sql_proxy
+    "private" = module.sql_private
   }
+}
+
+resource "azurerm_mssql_database" "db" {
+  for_each = local.server
 
   name           = "test-${each.key}"
-  server_id      = each.value
+  server_id      = each.value.server_id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
   max_size_gb    = 4
@@ -14,4 +18,12 @@ resource "azurerm_mssql_database" "db" {
   sku_name       = "BC_Gen5_2"
   zone_redundant = true
   tags           = local.tags
+}
+
+resource "azurerm_key_vault_secret" "connection_strings" {
+  for_each = azurerm_mssql_database.db
+
+  key_vault_id = module.configuration.key_vault_id
+  name         = "${each.value.name}-connection-string"
+  value        = "Server=tcp:${local.server[each.key].fully_qualified_domain_name},1433;Initial Catalog=${each.value.name};Persist Security Info=False;User ID=${local.server[each.key].administrator_login};Password=${local.server[each.key].administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 }
