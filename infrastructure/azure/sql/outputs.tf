04fac0dcee7e7c12a6d4249777864b811fc9b261
Jim Counts <jamesrcounts@outlook.com>
2021-09-12 00:30:57 +0000

    Update SQL server outputs

--- infrastructure/azure/sql/outputs.tf
+++ infrastructure/azure/sql/outputs.tf
@@ -1,3 +1,16 @@
+output "administrator_login" {
+  value = azurerm_mssql_server.server.administrator_login
+}
+
+output "administrator_login_password" {
+  value     = azurerm_mssql_server.server.administrator_login_password
+  sensitive = true
+}
+
+output "fully_qualified_domain_name" {
+  value = azurerm_mssql_server.server.fully_qualified_domain_name
+}
+
 output "server_id" {
   value = azurerm_mssql_server.server.id
 }
